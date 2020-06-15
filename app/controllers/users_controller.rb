12e6d0dd5d3d4bc7ed60e7e39f62be67735f0b77
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-06-15 03:00:37 +0000

    edit etc(6/15)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,64 +1,71 @@
 class UsersController < ApplicationController
-before_action :authenticate_user!
-def index
 
-	@users = User.all
-  @book = Book.new
+    before_action :authenticate_user!
 
-end
-def new
+    def index
 
-	@user = User.new
+      	@users = User.all
+        @book = Book.new
 
-end
-def edit
-
-	@user = User.find(params[:id])
-
-    if @user.id == current_user.id
-       render 'edit'
-    else
-      redirect_to new_user_session_path
     end
 
-end
+    def new
 
-def show
+    	 @user = User.new
 
-  @user = User.find(params[:id])
-  @book = Book.new
-  @books = @user.books
-
-end
-
-def create
-
-	@user = User.new(name: params[:name], email: params[:email],image_name: "default_user.jpg",
-      password: params[:password])
-
-    if @user.save
-      flash[:notice] = "Welcome! You have signed up successfully."
-      redirect_to user_path(@user.id)
-    else
-      redirect_to user_new_path
     end
-end
 
-def update
+    def edit
 
-    @user = User.find_by(id: params[:id])
-    @user.update(user_params)
-    
-    if @user.save
-      flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(@user.id)
-    else
-      @users = User.all
-      render 'edit'
+      	@user = User.find(params[:id])
+
+            if @user.id == current_user.id
+               render 'edit'
+            else
+              redirect_to new_user_session_path
+            end
+
     end
-end
-private
-def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-end
+
+    def show
+
+        @user = User.find(params[:id])
+        @book = Book.new
+        @books = @user.books
+
+    end
+
+    def create
+
+      	@user = User.new(name: params[:name], email: params[:email],image_name: "default_user.jpg",
+            password: params[:password])
+
+            if @user.save
+              flash[:notice] = "Welcome! You have signed up successfully."
+              redirect_to user_path(@user.id)
+            else
+              redirect_to user_new_path
+            end
+    end
+
+    def update
+
+        @user = User.find_by(id: params[:id])
+        @user.update(user_params)
+
+            if @user.save
+              flash[:notice] = "You have updated user successfully."
+              redirect_to user_path(@user.id)
+            else
+              @users = User.all
+              render 'edit'
+            end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
+    end
+
 end
